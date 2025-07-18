1a77455d493d5b4c25e9a2556c15768e66305b2b
Shorya Dubey <shoryadubey7610@gmail.com>
2025-07-18 16:30:08 +0530

    some changes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
     serverActions: {},
-  }
+  },
+
+  devIndicators: {
+    buildActivity: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
